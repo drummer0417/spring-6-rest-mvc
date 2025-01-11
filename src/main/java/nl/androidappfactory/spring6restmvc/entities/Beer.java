769e29d009c14c11fdba6125efea32811da5b0f2
Hans <drummer0417@gmail.com>
2025-01-11 09:57:33 +0100

    Added MySQL configuration

--- src/main/java/nl/androidappfactory/spring6restmvc/entities/Beer.java
+++ src/main/java/nl/androidappfactory/spring6restmvc/entities/Beer.java
@@ -3,7 +3,9 @@ package nl.androidappfactory.spring6restmvc.entities;
 import jakarta.persistence.*;
 import lombok.*;
 import nl.androidappfactory.spring6restmvc.model.BeerStyle;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -22,7 +24,8 @@ public class Beer {
     @Id
     @GeneratedValue(generator = "UUID")
     @UuidGenerator
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID id;
 
     @Version
